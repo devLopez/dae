5101f4d7f4a835465cabffe02c2b33a719ae51b5
Matheus Lopes Santos <fale_com_lopez@hotmail.com>
2021-02-02 12:55:12 -0300

    Refatoração da geração do código de barras

--- src/Factories/ImageFactory.php
+++ src/Factories/ImageFactory.php
@@ -2,40 +2,29 @@
 
 namespace Igrejanet\DAE\Factories;
 
-use Zend\Barcode\Renderer\Image;
+use Laminas\Barcode\Renderer\Image;
 
 /**
  * ImageFactory
  *
  * @author  Matheus Lopes Santos <fale_com_lopez@hotmail.com>
- * @version 1.0.0
- * @since   04/01/2019
+ * @version 2.0.0
  * @package Igrejanet\Dae\Factories
  */
 class ImageFactory
 {
-    /**
-     * @param   Image  $image
-     * @return  false|string|Image
-     */
-    public static function make(Image $image)
+    public static function make(Image $image): string
     {
-        $dir = sys_get_temp_dir();
+        $renderer = $image->draw();
 
-        if ( ! is_writable($dir) ) {
-            $dir = __DIR__ . '/../../resources/barcode';
-        }
+        ob_start();
 
-        $file       = $dir . '/barcode.jpg';
-        $renderer   = $image->draw();
+        imagejpeg($renderer, null, 100);
 
-        imagejpeg($renderer, $file, 100);
-
-        $image = file_get_contents($file);
+        $image = ob_get_clean();
         $image = base64_encode($image);
 
         imagedestroy($renderer);
-        unlink($file);
 
         return $image;
     }
